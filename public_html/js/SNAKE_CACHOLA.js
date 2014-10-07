62a492373bb0304da8dea8a715f7de8705015b47
aaron cachola <miakelcachola@hotmail.com>
2014-10-07 15:01:00 -0700

    update

--- public_html/js/SNAKE_CACHOLA.js
+++ public_html/js/SNAKE_CACHOLA.js
@@ -1,7 +1,16 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
+ var snake;
+ 
+ 
+ var context;
+ var screenHeight;
+ var screenWidth;
+ 
+ function gameInitalize(){
+     
+ }
+function gameLoop(){
+    
+}
+function gameDraw(){
+    
+}

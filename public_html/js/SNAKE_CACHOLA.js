b98abc8e2d7d4e1d192a39495b71296a7a0d3b96
aaron cachola <miakelcachola@hotmail.com>
2014-10-09 14:59:52 -0700

    update

--- public_html/js/SNAKE_CACHOLA.js
+++ public_html/js/SNAKE_CACHOLA.js
@@ -6,11 +6,17 @@
  var screenWidth;
  
  function gameInitalize(){
-     
+     var canvas = document.getElementById("game-screen");
+     context=canvas.getContext("2d");
+     screenWidth= window.innerWidth;
+     screenHeight= window.innerHeight;
+     canvas.width= screenWidth;
+     canvas.height=screenHeight;
  }
 function gameLoop(){
     
 }
 function gameDraw(){
-    
+    context.fillStyle="rgb(134,36,235)";
+    context.fillRect();
 }
